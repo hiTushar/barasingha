d2a2783fa7ae32f9caa34b2d11aa0b77f5a772ec
tusharMF <tushar.jain@monsoonfish.com>
2023-12-13 17:12:48 +0530

    Add Lazy loading for homepage

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import './Loader.scss';
+const Loader = () => {
+    return (
+        <div className='loader-container'>
+          <div className='loader'>
+            <div></div><div></div> 
+          </div>
+        </div>
+    )
+}
+
+export default Loader;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import Banner from '../../components/pages/Banner';
-import Environment from '../../components/sections/Environment/Environment';
-import Security from '../../components/sections/Security/Security';
-import Solutions from '../../components/sections/Solutions/Solutions';
-import Certifications from '../../components/sections/Certifications/Certifications';
-import Process from '../../components/sections/Process/Process';
-import Interested from '../../components/sections/Interested/Interested';
-import Partners from '../../components/sections/Partners/Partners';
-import Packages from '../../components/sections/Packages/Packages';
 import video from "../../media/ball.mp4";
+import Loader from '../../components/Loader';
+
+const Environment = lazy(() => import('../../components/sections/Environment/Environment'));
+const Security = lazy(() => import('../../components/sections/Security/Security'));
+const Packages = lazy(() => import('../../components/sections/Packages/Packages'))
+const Solutions = lazy(() => import('../../components/sections/Solutions/Solutions'));
+const Certifications = lazy(() => import('../../components/sections/Certifications/Certifications'));
+const Process = lazy(() => import('../../components/sections/Process/Process'));
+const Partners = lazy(() => import('../../components/sections/Partners/Partners'));
+const Interested = lazy(() => import('../../components/sections/Interested/Interested'));
 
 function Home({ 
                setShowContactUsModal = () => {},
@@ -28,19 +30,35 @@ function Home({
                  setShowSendingFailed={setShowSendingFailed}
                  setShowWrongPassword={setShowWrongPassword}
                />
-         <Environment />
+         <Suspense fallback={<Loader />}>
+            <Environment />
+         </Suspense>
          <div className="env_video_container">
             <div className="env_video">
                <video disableRemotePlayback src={video} autoPlay loop muted playsInline></video>
             </div>
          </div >
-         <Security />
-         <Packages />
-         <Solutions />
-         <Certifications />
-         <Process />
-         <Partners />
-         <Interested setShowContactUsModal={setShowContactUsModal} />
+         <Suspense fallback={<Loader />}>
+            <Security />
+         </Suspense>
+         <Suspense fallback={<Loader />}>
+            <Packages />
+         </Suspense>
+         <Suspense fallback={<Loader />}>
+            <Solutions />
+         </Suspense>
+         <Suspense fallback={<Loader />}>
+            <Certifications />
+         </Suspense>
+         <Suspense fallback={<Loader />}>
+            <Process />
+         </Suspense>
+         <Suspense fallback={<Loader />}>
+            <Partners />
+         </Suspense>
+         <Suspense fallback={<Loader />}>
+            <Interested setShowContactUsModal={setShowContactUsModal} />
+         </Suspense>
       </>
    )
 }
