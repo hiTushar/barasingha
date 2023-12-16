4f6101a8f2b1b58b91c33a44bd45161ecbd3553f
tusharMF <tushar.jain@monsoonfish.com>
2023-12-16 15:42:21 +0530

    Font size fix

--- src/components/sections/Solutions/Solutions.js
+++ src/components/sections/Solutions/Solutions.js
@@ -30,7 +30,9 @@ function Solutions() {
                            <img src={cube} alt={cube} title={cube} className='mb-4' />
                            <div className="heading heading--two">Ransomware cyber threat at its worst</div>
                            <div className="para para--lg para--light my-2">While ransomware has been a nuisance for more than a decade, the scope for ransomware has expanded throughout the period of digital intensification brought by the COVID-19 pandemic.</div>
-                           <Link to="/solutionspage" className="solution_link mt-5">Need a Solution? <img src={arrow} alt={arrow} /></Link>
+                           <Link to="/solutionspage" className="solution_link mt-5">
+                              <span className='text'>Need a Solution?</span> 
+                              <img src={arrow} alt={arrow} /></Link>
                         </div>
                      </div>
                   </div>
@@ -43,7 +45,10 @@ function Solutions() {
                            <img src={krook} alt={krook} title={krook} className='mb-4' />
                            <div className="heading heading--two">KrØØk: Serious vulnerability affected encryption of billion+ Wi‑Fi devices</div>
                            <div className="para para--lg para--light my-2">KrØØk affects devices with Wi-Fi chips by Broadcom and Cypress that haven’t yet been patched. These are the most common Wi-Fi chips used in contemporary Wi-Fi capable devices such as smartphones, tablets, laptops, and IoT gadgets</div>
-                           <Link to="/solutionspage" className="solution_link mt-5">Need a Solution? <img src={arrow} alt={arrow} /></Link>
+                           <Link to="/solutionspage" className="solution_link mt-5">
+                              <span className='text'>Need a Solution?</span> 
+                              <img src={arrow} alt={arrow} />
+                           </Link>
                         </div>
                      </div>
                      <div className="col-5">
@@ -66,7 +71,10 @@ function Solutions() {
                            <img src={operation} alt={operation} title={operation} className='mb-4' />
                            <div className="heading heading--two">Operation Ghost: The Dukes aren’t back – they never left</div>
                            <div className="para para--lg para--light my-2">The Dukes have been in the spotlight after their suspected involvement in the breach of the Democratic National Committee in the run-up to the 2016 US elections.</div>
-                           <Link to="/solutionspage" className="solution_link mt-5">Need a Solution? <img src={arrow} alt={arrow} /></Link>
+                           <Link to="/solutionspage" className="solution_link mt-5">
+                              <span className='text'>Need a Solution?</span> 
+                              <img src={arrow} alt={arrow} />
+                           </Link>
                         </div>
                      </div>
                   </div>
