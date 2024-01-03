2e4a011442ec369851b0853e7849eedabba78511
tusharMF <tushar.jain@monsoonfish.com>
2024-01-03 17:35:35 +0530

    Fix contact us modal

--- src/components/pages/Forms/ContactUs.js
+++ src/components/pages/Forms/ContactUs.js
@@ -1,62 +1,65 @@
+/* eslint-disable no-unused-vars */
 import './ContactUs.scss';
 import head from '../../../media/ExtractedHead3.webp';
-import close from '../../../media/closeoutlined.svg';
 import { useState } from 'react';
 import SuccessModal from './Success';
 
-const ContactUsModal = ({closeContactUsModal}) => {
+const ContactUsModal = ({ closeContactUsModal }) => {
+    const [showSuccessModal, setShowSuccessModal] = useState(false);
+    const [showContactModal, setShowContactModal] = useState(true);
 
+    const submitForm = (event) => {
+        event.preventDefault();
+        console.log('submitted');
+        setShowSuccessModal(true);
+    }
 
-  const[showSuccessModal, setShowSuccessModal] =useState(false);
-  const [showContactModal, setShowContactModal] = useState(true);
-    
-    return(
-      <>
-      <div className="contact_us_wrapper"></div>
-      {showContactModal &&
-            <>
-               
-               <div className="contact_us_container">
-        <div className="left_container">
-            <div className="contact_us">Contact us</div>
-            <div className="send_us">Send us a message and we will get back ASAP!</div>
-            <div className="barasingha_head">
-                <img src={head} alt={head}/>
-            </div>
+    return (
+        <div className='contact_us_wrapper'>
+            {
+                !showSuccessModal ? (
+                    <div className='contact_us_container'>
+                        <div className='info'>
+                            <p className='info-title'>Contact us</p>
+                            <p className='info-subtext'>Send us a message and we will get back ASAP!</p>
+                            <div className='info-logo'>
+                                <img src={head} alt={'logo'} />
+                            </div>
+                        </div>
+                        <div className='form'>
+                            <form onSubmit={submitForm}>
+                                <div className='form-field'>
+                                    <p className='form-field-title'>
+                                        Name<span className='mandatory'>*</span>
+                                    </p>
+                                    <input className='form-field-input' type='text' placeholder='Enter your name' required></input>
+                                </div>
+                                <div className='form-field'>
+                                    <p className='form-field-title'>
+                                        Email<span className='mandatory'>*</span>
+                                    </p>
+                                    <input className='form-field-input' type='email' placeholder='Enter your email' required></input>
+                                </div>
+                                <div className='form-field'>
+                                    <p className='form-field-title'>
+                                        Message
+                                    </p>
+                                    <textarea className='form-field-textarea' placeholder='Type your message here'></textarea>
+                                    <p className='form-field-input-subtext'>Maximum 500 characters</p>
+                                </div>
+                                <div className='form-field-submit'>
+                                    <button type='submit'>SEND MESSAGE</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className='close' onClick={closeContactUsModal}>&times;</div>
+                    </div>
+                ) : (
+                    <SuccessModal closeContactUsModal={closeContactUsModal} />
+                )
+            }
         </div>
-        <div className="right_container">
-            <div className="close" onClick={closeContactUsModal}>
-                <img src={close} alt="close"></img>
-            </div>
-            <div className="input_name">
-                Name <div className="mandatory">*</div>
-            </div>
-            <div className="contact_us_bar pad_bot">
-                <input class="contactus_input" type="text"></input>
-            </div>
-            <div className="input_name">
-                Email <div className="mandatory">*</div>
-            </div>
-            <div className="contact_us_bar pad_bot">
-                <input class="contactus_input" type="text"></input>
-            </div>
-            <div className="input_name">
-                Message
-            </div>
-            <div className="contact_us_bar">
-                <input class="contactus_message" type="text"></input>
-            </div>
-            <div className="max_character">
-                Maximum 2000 characters
-            </div>
-            <button className="send_message_container" onClick={() => { setShowSuccessModal(true); setShowContactModal(false) }}>SEND MESSAGE</button>
-        </div>
-        </div>
-            </>
-         }
-         {showSuccessModal && <SuccessModal closeContactUsModal={closeContactUsModal} />}
-      </>
     );
-  }
+}
 
-  export default ContactUsModal;
+export default ContactUsModal;
