e0b9d2f33c1fc61faab535245535a236b4bef0fe
tusharMF <tushar.jain@monsoonfish.com>
2024-01-20 19:44:54 +0530

    Responsive process section homepage

--- src/components/sections/Process/Process.js
+++ src/components/sections/Process/Process.js
@@ -5,14 +5,16 @@ import video from '../../../media/process.webm'
 function Process() {
    return (
       <div className="process_container">
-         <div className="container">
-            <div className="process_heading">PROCESS</div>
-            <div className="work">How it Works?</div>
+         <div className="component-container">
+            <div className='title-wrapper'>
+               <p className="heading heading--fancy">PROCESS</p>
+               <p className="heading heading--two">How it Works?</p>
+            </div>
             <div className="video_container mx-auto">
                <video disableRemotePlayback className="video" src={video} autoPlay loop muted playsInline/>
             </div>
-            <div className="process-ellipse"></div>
          </div>
+         <div className="process-ellipse"></div>
       </div>
    );
 }
