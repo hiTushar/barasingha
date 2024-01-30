dc5dbc61144e2551bdc6c6127504e559f4b519ef
tusharMF <tushar.jain@monsoonfish.com>
2024-01-30 20:23:43 +0530

    Faq section fix: - font size and layout fix - scroll the tapped faq to top of the screen in mobile view

--- src/components/pages/Common/FAQ/FAQ.js
+++ src/components/pages/Common/FAQ/FAQ.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './FAQ.scss';
 import video from "../../../../media/ball.webm";
 import video2 from "../../../../media/faq-ball.webm";
@@ -10,27 +10,35 @@ function Faq({ setShowContactUsModal = () => { } }) {
    const tabData = [
       {
          title: 'What is the difference between Supreme and Premium Suite?',
-         content: '<div className="tab-content-inner-box"><div className="tab-content-text">1. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
+         content: '<div className="tab-content-inner-box"><div className="tab-content-text">1. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><br/><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
       },
       {
          title: 'How can I purchase a Suite of products?',
-         content: '<div className="tab-content-inner-box"><div className="tab-content-text">2. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
+         content: '<div className="tab-content-inner-box"><div className="tab-content-text">2. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><br/><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
       },
       {
          title: 'For how many devices is Supreme Suite applicable?',
-         content: '<div className="tab-content-inner-box"><div className="tab-content-text">3. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
+         content: '<div className="tab-content-inner-box"><div className="tab-content-text">3. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><br/><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
       },
       {
          title: 'How do you provide support?',
-         content: '<div className="tab-content-inner-box"><div className="tab-content-text">4. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
+         content: '<div className="tab-content-inner-box"><div className="tab-content-text">4. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><br/><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
       },
       {
          title: 'How can I make payment?',
-         content: '<div className="tab-content-inner-box"><div className="tab-content-text">5. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
+         content: '<div className="tab-content-inner-box"><div className="tab-content-text">5. Growing your tokens liquidity shouldn\'t be so hard. We’ve made it as simple three steps: create LP tokens, stake and earn. With options that like no lock to 180 day locks earning 50% more yield, Dapploy provides flexibility for developers and investors.</div><br/><ul className="tab-content-list"><li>Growing your tokens liquidity shouldn\'t be so hard.</li><li>We’ve made it as simple three steps: create LP tokens, stake and earn.</li></ul><div className="tab-content-fade-text">Meet the needs of your business with Security solutions to evolve your organization</div></div>'
       },
    ];
 
    const [activeTab, setActiveTab] = useState(0);
+   const tabRef = useRef(null);
+
+   useEffect(() => {
+      if(window.innerWidth < 992 && activeTab !== null && tabRef.current) {
+         const { top } = tabRef.current.getBoundingClientRect();
+         window.scrollBy(0, top - 65, { behavior: 'smooth' });
+      }
+   }, [activeTab])
 
    function getCurrentDimension() {
       if (window.innerWidth < 992) {
@@ -39,12 +47,15 @@ function Faq({ setShowContactUsModal = () => { } }) {
       return false;
    }
 
-   const changeActiveTab = (index) => {
+   const changeActiveTab = (index, element) => {
       if(index === activeTab && getCurrentDimension()) {
          setActiveTab(null);
       }
       else {
          setActiveTab(index);
+         if(window.innerWidth < 992) {
+            tabRef.current = element;
+         }
       }
    }
    
@@ -60,19 +71,21 @@ function Faq({ setShowContactUsModal = () => { } }) {
                      {tabData.map((tab, index) => (
                         <div
                            key={index}
-                           onClick={() => changeActiveTab(index)}
                            disabled={tab.disabled}
                            className='faq-box'
                         // dangerouslySetInnerHTML={{ __html: tab.title }}
                         >
-                           <div className={`nav-card ${activeTab === index ? 'active' : ''}`}>
+                           <div 
+                              className={`nav-card ${activeTab === index ? 'active' : ''}`}
+                              onClick={(e) => changeActiveTab(index, e.target)}
+                           >
                               <span className='tab-title'>{tab.title}</span>
                               <span className='tab-arrow'>
                                  <img src={tabArrow} alt='tab arrow' />
                               </span>
                            </div>
                            <div
-                              key={index}
+                              // key={index}
                               className={`tab-pane ${activeTab === index ? 'view-tab' : 'hide-tab'}`}
                               dangerouslySetInnerHTML={{ __html: tab.content }}
                            >
